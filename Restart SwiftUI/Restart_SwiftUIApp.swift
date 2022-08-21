409c52efdeb9e1750feceb463096fefb0d3fb5d4
atakanapan <atakanapan12@gmail.com>
2022-08-21 20:08:19 +0300

    Initial Commit

---
+++ Restart SwiftUI/Restart_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_SwiftUIApp.swift
+//  Restart SwiftUI
+//
+//  Created by Atakan Apan on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Restart_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
